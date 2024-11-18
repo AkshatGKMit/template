3a31500c2e13f3c5d12b5321106c4bc7c8abe76d
AkshatGKMit <akshat@gkmit.co>
2024-11-18 11:27:38 +0530

    feat: Setup Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { useContext, useState } from 'react';
-import { Button, ScrollView, Switch, Text } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
+import { Button, ScrollView, StatusBar, Switch, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import ThemeContext, { ThemeContextProvider } from '@config/ThemeContext';
 import GradientScreen from '@components/gradientScreen';
 import BottomSheet from '@components/bottomSheet';
 import Toast from 'react-native-toast-message';
+import { NavigationContainer } from '@react-navigation/native';
+import { GlobalThemedStyles } from '@themes/globalStyles';
+import Splash from '@screens/splash/Splash';
+import { Colors } from '@themes';
 
 const App = () => {
   return (
@@ -22,65 +26,30 @@ const App = () => {
 };
 
 const Main = () => {
-  const { theme, switchThemeMode, safeAreaInsets: insets } = useContext(ThemeContext);
+  const [showSplashScreen, setShowSplashScreen] = useState(true);
 
-  function _onSwitchTheme() {
-    switchThemeMode(theme.isDark ? 'light' : 'dark');
-  }
+  const [s, SS] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplashScreen(false);
+    }, 30);
+  }, []);
+
+  const globalStyles = GlobalThemedStyles();
 
   return (
-    <GradientScreen style={{ padding: 20 }}>
-      <Switch
-        value={theme.isDark}
-        onValueChange={_onSwitchTheme}
-      />
-
-      <Button
-        title="Show Scrolling Bottom Sheet"
-        onPress={() =>
-          BottomSheet.show({
-            child: (
-              <ScrollView style={{ backgroundColor: 'red', width: '100%' }}>
-                {Array.from({ length: 100 }).map((_: unknown, i) => (
-                  <Text
-                    key={i}
-                    style={{ color: theme.colors.text }}
-                  >
-                    {i}
-                  </Text>
-                ))}
-              </ScrollView>
-            ),
-            snap: {
-              closingPoint: 0.8,
-              points: [0.1, 0.25, 0.5, 0.75],
-            },
-          })
-        }
-      />
-
-      <Button
-        title="Show Normal Bottom Sheet"
-        onPress={() =>
-          BottomSheet.show({
-            child: (
-              <>
-                {Array.from({ length: 10 }).map((_: unknown, i) => (
-                  <Text style={{ color: theme.colors.text }}>{i}</Text>
-                ))}
-              </>
-            ),
-            borderRadius: 12,
-          })
-        }
-      />
-
-      <Button
-        title="Toast"
-        onPress={() => Toast.show({ text1: 'This is toast' })}
-      />
-
-      {/* {showBottomSheet && <BottomSheet />} */}
+    <GradientScreen showStatusBar={s}>
+      <NavigationContainer>
+        {showSplashScreen ? (
+          <Splash />
+        ) : (
+          <Switch
+            value={s}
+            onValueChange={SS}
+          />
+        )}
+      </NavigationContainer>
     </GradientScreen>
   );
 };
