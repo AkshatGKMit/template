3a323edac709673ae891b45f5ba5b66cf2505b03
AkshatGKMit <akshat@gkmit.co>
2024-12-08 13:52:51 +0530

    chore: Add TODOS

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
-import { StatusBar, useColorScheme } from 'react-native';
+import { FlatList, StatusBar, Text, useColorScheme, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 
@@ -11,6 +11,8 @@ import store, { useAppDispatch, useAppSelector } from '@store';
 import { switchTheme } from '@store/reducers/theme';
 import { loginUser } from '@store/reducers/auth';
 import AppBar from '@components/appBar';
+import FastImage from 'react-native-fast-image';
+import GridView from '@components/gridView/GridView';
 
 const App = () => {
   useEffect(() => {}, []);
@@ -49,14 +51,29 @@ const ScreenAppBar = () => {
 const Main = () => {
   const colorScheme = useColorScheme();
   const dispatch = useAppDispatch();
+  const theme = useAppSelector(({ theme }) => theme.colors);
+
+  const [data, setData] = useState<any>();
+
+  const fetchData = async () => {
+    const dataRes: any = await fetch('https://dummyjson.com/products').then((res) => res.json());
+
+    setData(dataRes.products[0]);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   useEffect(() => {
     dispatch(switchTheme(colorScheme ?? ThemeMode.light));
   }, [colorScheme]);
 
+  const num = 3;
+
   return (
     <Scaffold
-      style={{ padding: 12, gap: 10 }}
+      style={{ padding: 12, gap: 10, flex: 1, flexDirection: 'row' }}
       appBar={<ScreenAppBar />}
     ></Scaffold>
   );
