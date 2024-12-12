5ab6a07925f09c7b1947856f90f5d30ea45a03a4
AkshatGKMit <akshat@gkmit.co>
2024-12-12 13:53:34 +0530

    chore: Installed redux saga

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -3,16 +3,9 @@ import { useNavigation } from '@react-navigation/native';
 import AppBar from '@components/appBar';
 import { ElevatedButton } from '@components/button';
 import Scaffold from '@components/scaffold';
+import useHeader from '@config/useHeader';
 import { Icons, ROUTES } from '@constants';
 import { globalStyles } from '@themes/globalStyles';
-import useHeader from '@config/useHeader';
-import { View } from 'react-native';
-import {
-  FilledIconButton,
-  IconButton,
-  OutlinedIconButton,
-  TonalIconButton,
-} from '@components/iconButton';
 
 const ScreenAppBar = () => {
   const { navigate } = useNavigation<StackNavigation>();
@@ -34,58 +27,17 @@ const ScreenAppBar = () => {
 
 const Home = () => {
   const { navigate } = useNavigation<StackNavigation>();
+
   useHeader<StackNavigation>(<ScreenAppBar />);
+
   const {
     PAGINATION: PAGINATION_ROUTE,
     INFINITE_PAGINATION: INFINITE_PAGINATION_ROUTE,
     FAVORITES: FAVORITES_ROUTE,
   } = ROUTES.STACK;
+
   return (
     <Scaffold style={{ padding: 12, gap: 10, flex: 1, ...globalStyles.columnCenter }}>
-      <View style={{ flexDirection: 'row', gap: 10 }}>
-        <IconButton
-          icon={Icons.materialIcons.window}
-          onPress={() => {}}
-        />
-        <IconButton
-          icon={Icons.materialIcons.window}
-          onPress={() => {}}
-          disabled
-        />
-      </View>
-      <View style={{ flexDirection: 'row', gap: 10 }}>
-        <FilledIconButton
-          icon={Icons.materialIcons.window}
-          onPress={() => {}}
-        />
-        <FilledIconButton
-          icon={Icons.materialIcons.window}
-          onPress={() => {}}
-          disabled
-        />
-      </View>
-      <View style={{ flexDirection: 'row', gap: 10 }}>
-        <TonalIconButton
-          icon={Icons.materialIcons.window}
-          onPress={() => {}}
-        />
-        <TonalIconButton
-          icon={Icons.materialIcons.window}
-          onPress={() => {}}
-          disabled
-        />
-      </View>
-      <View style={{ flexDirection: 'row', gap: 10 }}>
-        <OutlinedIconButton
-          icon={Icons.materialIcons.window}
-          onPress={() => {}}
-        />
-        <OutlinedIconButton
-          icon={Icons.materialIcons.window}
-          onPress={() => {}}
-          disabled
-        />
-      </View>
       <ElevatedButton
         label={PAGINATION_ROUTE}
         onPress={() => navigate(PAGINATION_ROUTE)}
